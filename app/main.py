bf89450229197c1643ba3aeaf219efb47f75abaa
Rimbozero <rimbozero@gmail.com>
2026-07-13 19:08:26 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,29 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people: list[dict]) -> list[Person]:
+    Person.people = {}
+
+    person_list = []
+
+    # First create all Person instances
+    for person in people:
+        person_list.append(Person(person["name"], person["age"]))
+
+    # Then add wife/husband links
+    for person_dict in people:
+        person = Person.people[person_dict["name"]]
+
+        if "wife" in person_dict and person_dict["wife"] is not None:
+            person.wife = Person.people[person_dict["wife"]]
+
+        if "husband" in person_dict and person_dict["husband"] is not None:
+            person.husband = Person.people[person_dict["husband"]]
+
+    return person_list
